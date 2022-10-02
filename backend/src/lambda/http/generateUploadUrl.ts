7c677b717dcbf80f52bdf37d24e712221ef7c6d6
Faheem Ismail <femo.ismail@gmail.com>
2022-10-02 11:38:57 +0200

    updated

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,26 +1,36 @@
 import 'source-map-support/register'
+import * as uuid from 'uuid'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import * as middy from 'middy'
-import { cors, httpErrorHandler } from 'middy/middlewares'
-
-import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
+import { generateUploadUrl, updateAttachmentUrl } from '../../helpers/todos'
 import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('generateUploadUrl')
 
-export const handler = middy(
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
-    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  logger.info('Generate UploadUrl event', { event })
 
-    return undefined
-  }
-)
+  const userId = getUserId(event)
+  const todoId = event.pathParameters.todoId
+  const attachmentId = uuid.v4()
 
-handler
-  .use(httpErrorHandler())
-  .use(
-    cors({
-      credentials: true
+  const uploadUrl = await generateUploadUrl(attachmentId)
+
+  await updateAttachmentUrl(userId, todoId, attachmentId)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      uploadUrl
     })
-  )
+  }
+}
